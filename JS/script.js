e4b08a7fc19304318c21aad4b192d7e1470466f5
francescotroiano98 <francesco-troiano-98@libero.it>
2023-06-13 16:41:07 +0200

    added one email with api

--- JS/script.js
+++ JS/script.js
@@ -3,12 +3,30 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
+      userEmail : '',
       
   }
 },
   methods: {
 
+    
+
 
   },
+
+  created(){
+    // ? this hook is executed at the creation of this Vue instance
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then( (response) => {
+        // § se e quando il server risponde
+        const result = response.data;
+
+        console.log(result.response);
+        
+        this.userEmail = result.response;
+
+    });
+
+}
 
 }).mount('#app');
