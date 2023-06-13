0edad9ee16a4930698c0199b2f4ee77e5cf79414
francescotroiano98 <francesco-troiano-98@libero.it>
2023-06-13 17:03:51 +0200

    Added 10 mails to the dom

--- JS/script.js
+++ JS/script.js
@@ -4,6 +4,7 @@ createApp({
   data() {
     return {
       userEmail : '',
+      userEmails: [],
       
   }
 },
@@ -15,17 +16,25 @@ createApp({
   },
 
   created(){
+    for(let i = 0; i<10; i++){
     // ? this hook is executed at the creation of this Vue instance
     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
     .then( (response) => {
         // § se e quando il server risponde
-        const result = response.data;
-
-        console.log(result.response);
         
-        this.userEmail = result.response;
+            const result = response.data;
+
+            console.log(result.response);
+            
+            this.userEmail = result.response;
+
+            this.userEmails.push(this.userEmail);
+          
+
+
 
     });
+} 
 
 }
 
